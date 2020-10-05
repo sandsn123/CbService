f1cd2e5cf9e299c06731ecd932c4e7a7fbd68b17
sai_git <330770597@qq.com>
2020-10-05 22:48:28 +0800

    complete

---
+++ CbService/CbService.h
@@ -0,0 +1,18 @@
+//
+//  CbService.h
+//  CbService
+//
+//  Created by sai on 2020/10/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CbService.
+FOUNDATION_EXPORT double CbServiceVersionNumber;
+
+//! Project version string for CbService.
+FOUNDATION_EXPORT const unsigned char CbServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CbService/PublicHeader.h>
+
+
